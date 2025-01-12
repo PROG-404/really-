2812b1a8d016946f10c9d0cc0f44d7631124566c
PROG-404 <appera8900@gmail.com>
2025-01-12 11:56:12 +0300

    Update BetaLip.lua

--- BetaLip.lua
+++ BetaLip.lua
@@ -1,2 +1,17 @@
 local ProtLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/PROG-404/really-/refs/heads/main/Library.lua"))()
 local mainWindow = ProgLib.UI.Window.new({    Title = "Window Prog",  Size = UDim2.new(0, 600, 0, 400),  Position = UDim2.new(0.5, -300, 0.5, -200), ProgLib:setTheme("Dark")})
+
+-- Value 
+_G.AutoClick = true
+
+-- Function 
+
+Function AutoClick
+  _G.AutoClick == true 
+	--server
+end)
+local mainButton = ProgLib.UI.Components.Button.new({
+    Text = "Remote",
+    Size = UDim2.new(0, 200, 0, 40),
+    Position = UDim2.new(0.5, -100, 0.2, 0),
+    OnClick = function()
