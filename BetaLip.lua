16fd31a2704dcd8b1868d08a908e7801cba836dd
PROG-404 <appera8900@gmail.com>
2025-01-12 11:57:11 +0300

    Update BetaLip.lua

--- BetaLip.lua
+++ BetaLip.lua
@@ -2,7 +2,7 @@ local ProtLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/PROG-
 local mainWindow = ProgLib.UI.Window.new({    Title = "Window Prog",  Size = UDim2.new(0, 600, 0, 400),  Position = UDim2.new(0.5, -300, 0.5, -200), ProgLib:setTheme("Dark")})
 
 local RunService = game:GetService("RunService")
-
+local Players = game:GetService("Players")
 -- Value 
 _G.AutoClick = true
 
