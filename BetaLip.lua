0b3997274bf606f272ec8a51f49d3a7bffb086ec
PROG-404 <appera8900@gmail.com>
2025-01-12 11:41:34 +0300

    Update BetaLip.lua

--- BetaLip.lua
+++ BetaLip.lua
@@ -1,1 +1,2 @@
 local ProtLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/PROG-404/really-/refs/heads/main/Library.lua"))()
+local mainWindow = ProgLib.UI.Window.new({    Title = "Window Prog",  Size = UDim2.new(0, 600, 0, 400),  Position = UDim2.new(0.5, -300, 0.5, -200) , })
