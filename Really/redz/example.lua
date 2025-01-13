b885e84d1609742b55c7a03ec1c3e74dfdf62982
PROG-404 <appera8900@gmail.com>
2025-01-13 18:20:51 +0300

    Update example.lua

--- Really/redz/example.lua
+++ Really/redz/example.lua
@@ -1,1 +1,6 @@
-
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/RedzLibV5/refs/heads/main/Source.lua"))()
+local window = redzlib:MakeWindow({
+    Name = "Blox peta", 
+    SubTitle = "Porg",
+    SaveFolder = ""
+})
