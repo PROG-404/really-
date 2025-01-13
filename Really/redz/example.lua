9a3011e0063e3420adf3910398d651f93586686c
PROG-404 <appera8900@gmail.com>
2025-01-13 18:24:52 +0300

    Update example.lua

--- Really/redz/example.lua
+++ Really/redz/example.lua
@@ -2,5 +2,20 @@ local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/realr
 local window = redzlib:MakeWindow({
     Name = "Blox peta", 
     SubTitle = "Porg",
-    SaveFolder = ""
+    SaveFolder = "example.json"
+})
+
+local Maintab = window:MakeTab({
+    Name = "Main",
+    Icon = "rbxassetid://123456789" -- icons 
+})
+
+Maintab:AddSection({
+    Name = "Main"
+})
+
+Maintab:AddDiscordInvite({
+    Name = "Invite",
+    Logo = "rbxassetid://123456789", -- icon roblox 
+    Invite = "https://discord.gg/BnYk2BtZ" -- link discord 
 })
