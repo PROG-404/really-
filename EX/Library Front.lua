53f92555567a4e7ed8daffd578131aba58c3fb20
PROG-404 <appera8900@gmail.com>
2025-01-28 22:04:03 +0300

    Update Library Front.lua

--- EX/Library Front.lua
+++ EX/Library Front.lua
@@ -1,11 +1,22 @@
-local Library = require("https://raw.githubusercontent.com/PROG-404/really-/refs/heads/main/Probably.lua")
+local Library = require(script.Parent.RedZLibrary)
 
 -- Create new library instance
 local gui = Library.new()
 
 -- Create window with custom config
 local window = gui:CreateWindow({
-    title = "FRONT LIBRARY",
+    title = "RedZ Library",
+    size = UDim2.new(0, 400, 0, 300),
+    theme = {
+        background = Color3.fromRGB(25, 25, 25),
+        foreground = Color3.fromRGB(30, 30, 30),
+        accent = Color3.fromRGB(0, 170, 255),
+        text = Color3.fromRGB(255, 255, 255),
+        buttons = {
+            close = Color3.fromRGB(255, 0, 0),
+            minimize = Color3.fromRGB(50, 50, 50)
+        }
+    }
 })
 
 -- Add buttons or other elements
