f92d9ac207f7b3c2ada78ae3eac4b523354465b1
PROG-404 <appera8900@gmail.com>
2025-01-29 18:45:11 +0300

    Update Library Front.lua

--- EX/Library Front.lua
+++ EX/Library Front.lua
@@ -1,8 +1,5 @@
 local FrontLip = loadstring(game:HttpGet("https://raw.githubusercontent.com/PROG-404/really-/refs/heads/main/Model.lua"))()
 
-local gui = FrontLip.new()
-
 local window = gui:CreateWindow({
-    title = "Example Window",
-    size = UDim2.new(0, 400, 0, 300)
+    title = "FRONT"
 })
